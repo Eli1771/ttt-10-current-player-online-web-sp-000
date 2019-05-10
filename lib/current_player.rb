bac45e9dbf670754efce06407e2fc43ef60fb2df
elijahmm17 <elijahmm17@gmail.com>
2019-05-10 15:45:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
 def turn_count(board)
-  
+  turn = 0
+  board.each do |space| 
+    if space == "X" || space == "O"
+      turn += 1 
+    end 
+  end
+  return turn
 end
 
 def current_player(board)
